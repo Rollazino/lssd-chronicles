01de5f1476b4caa7b91bf2da4d9892beef3a1640
FreezikCZ <slouppavel@seznam.cz>
2026-09-16 21:21:51 +0200

    gg
    g

--- src/lib/remembrance.ts
+++ src/lib/remembrance.ts
@@ -3,5 +3,5 @@ export const isSeptemberRemembrancePeriod = () => {
   const month = today.getMonth();
   const day = today.getDate();
 
-  return month === 8 && day >= 1 && day <= 15;
+  return month === 8 && day >= 1 && day <= 18;
 };
